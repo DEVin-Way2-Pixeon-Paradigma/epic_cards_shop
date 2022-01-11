89f9aa6822cad1dfba86b724e30757c8add8b1ec
douglas-cavalcante <henriquedouglas2803@gmail.com>
2022-01-11 19:51:56 -0300

    adiciona navegacao

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
-import { useEffect, useState, useRef } from 'react';
+
 import './App.css';
-import Card from './Components/Card';
+import {Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
-
-  const inputRef = useRef(null)
-  const [cards, setCards] = useState([])
-
-  //Query params 
-  useEffect(() => {
-
-    async function handleGetCard() {
-      const response = await fetch('http://localhost:3333/cards?_limit=18')
-      const data = await response.json()
-      setCards(data)
-    }
-
-    handleGetCard();
-
-    
-
-  }, [])
-
-
-
-  return (
-    <div>
-      <input placeholder='Pesquisa'   ref={inputRef}/>
-      <h1>Total de cards: {cards.length}</h1>
-      <div className='container'>
-        {
-          cards.map((card) =>
-            <Card
-              name={card.name}
-              image={card.card_images[0].image_url_small}
-              type={card.type}
-            />
-          )
-        }
-      </div>
-
-
-    </div>
-  );
+   return (
+     <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About />} />
+     </Routes>
+   )
 }
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,41 @@
+import { useEffect, useState, useRef } from 'react';
+
+import Card from '../../components/Card';
+
+function Home() {
+
+  const inputRef = useRef(null)
+  const [cards, setCards] = useState([])
+
+  //Query params 
+  useEffect(() => {
+
+    async function handleGetCard() {
+      const response = await fetch('http://localhost:3333/cards?_limit=18')
+      const data = await response.json()
+      setCards(data)
+    }
+
+    handleGetCard();
+  }, [])
+
+  return (
+    <div>
+      <input placeholder='Pesquisa'   ref={inputRef}/>
+      <h1>Total de cards: {cards.length}</h1>
+      <div className='container'>
+        {
+          cards.map((card) =>
+            <Card
+              name={card.name}
+              image={card.card_images[0].image_url_small}
+              type={card.type}
+            />
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Home;
