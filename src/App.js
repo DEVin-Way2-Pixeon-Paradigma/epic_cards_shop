f330d0909a4c03f6efad5a4ad24e5aacb439b07b
douglas-cavalcante <henriquedouglas2803@gmail.com>
2022-01-11 21:24:38 -0300

    adiciona menu da aplicação

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cards from './pages/Cards';
 import About from './pages/About';
+import Menu from './components/Menu';
 
 function App() {
-   return (
-     <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/cards" element={<Cards />} />
-       <Route path="/about" element={<About />} />
-     </Routes>
-   )
+  return (
+    <>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Menu() {
+  return (
+    <nav className='menu'>
+      <h1 className='menu-title'>Epic Cards Shop</h1>
+      <ul className='menu-list'>
+        <li><Link to="/" className='menu-list-item'>Home</Link></li>
+        <li><Link to="/cart" className='menu-list-item' >Cart</Link></li>
+        <li><Link to="/about" className='menu-list-item'>Sobre</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
